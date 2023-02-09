f5d7bc547a1cb2107284e0b5d372d612b63cb9a4
richelbilderbeek <richel@richelbilderbeek.nl>
2023-02-09 16:57:18 +0100

    Add index

--- scripts/create.sh
+++ scripts/create.sh
@@ -9,6 +9,27 @@
 #
 #   ./scripts/create.sh
 #
+
+
+if [ ! -f "svmult.cls" ]; then
+  cp style/svmult.cls svmult.cls
+fi
+
+
+if [ ! -f "svmult.cls" ]; then
+  echo "'svmult.cls' does not exist."
+  exit 42
+fi
+
+if [ ! -f "svind.ist" ]; then
+  cp style/author/svind.ist svind.ist
+fi
+
+if [ ! -f "svind.ist" ]; then
+  echo "'svind.ist' does not exist."
+  exit 43
+fi
+
 pdflatex header.tex
 bibtex header >/dev/null
 pdflatex header.tex >/dev/null
