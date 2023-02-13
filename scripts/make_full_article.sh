d192131cd0a5f80d9f1b88aca2e1252fcabc0775
richelbilderbeek <richel@richelbilderbeek.nl>
2023-02-13 09:10:25 +0100

    Use script in GHA to make full article

--- scripts/make_full_article.sh
+++ scripts/make_full_article.sh
@@ -4,7 +4,7 @@
 #
 #   ./scripts/make_full_article.sh
 #
-sed -i 's/^.iffalse$/% \iffalse/g' article.tex
-sed -i 's/^.fi$/% \fi/g' article.tex
-sed -i 's/^.iffalse$/% \iffalse/g' header.tex
-sed -i 's/^.fi$/% \fi/g' header.tex
+sed -i 's/^\\iffalse$/% \\iffalse/g' article.tex
+sed -i 's/^\\fi$/% \\fi/g' article.tex
+sed -i 's/^\\iffalse$/% \\iffalse/g' header.tex
+sed -i 's/^\\fi$/% \\fi/g' header.tex
